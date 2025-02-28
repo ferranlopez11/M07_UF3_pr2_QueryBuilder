bd056ce36dd2b1477322b02bbbd714b8a3eadec8
Ferran <pl2023316@365.stucom.com>
2025-02-28 18:36:18 +0100

    commit film year #2

--- database/seeders/films.php
+++ database/seeders/films.php
@@ -21,7 +21,7 @@ class films extends Seeder
         for ($i = 0; $i < 10; $i++) {
             DB::table('films')->insert([
                 'name' => $faker->unique()->sentence(3),
-                'year' => $faker->numberBetween(1980, 2024),
+                'year' => $faker->numberBetween(1900, 2024),
                 'genre' => $faker->randomElement($genres),
                 'country' => substr($faker->country(), 0, 30),
                 'duration' => $faker->numberBetween(60, 180),
